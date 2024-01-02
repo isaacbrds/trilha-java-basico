0d19c6d8a13c3de3be56d4f078eac5d9ab2860d0
isaac <isaac.brigido@gmail.com>
2024-01-02 11:18:10 -0300

    Criando Exemplo usando Foreach no controle de fluxo.

---
+++ controle-fluxo/src/ExemploForEach.java
@@ -0,0 +1,9 @@
+public class ExemploForEach {
+    public static void main(String[] args) {
+        String alunos [] = {"João", "Pedro", "Jose"};
+
+        for (String aluno : alunos) {
+            System.out.println("Aluno:" + aluno);
+        }
+    }
+}
