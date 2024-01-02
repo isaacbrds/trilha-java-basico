923714755035cac905ce68418bdd9b7f8ec434bf
isaac <isaac.brigido@gmail.com>
2024-01-02 10:31:01 -0300

    Controle de fluxo composto

---
+++ controle-fluxo/src/CaixaEletronico.java
@@ -0,0 +1,15 @@
+class CaixaEletronico{
+    public static void main(String[] args) {
+        double saldo = 25;
+        double valorSolicitado = 29;
+
+        if(valorSolicitado < saldo){
+            saldo -= valorSolicitado;
+            System.out.println(saldo);
+        }else{
+            System.out.println("Saldo insuficiente, deposite mais dinheiro");
+        }
+
+        
+    }
+}
