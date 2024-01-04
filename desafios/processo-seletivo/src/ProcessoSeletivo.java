6276159700e5b950aab5bd65248e9f5fa21d05dd
isaac <isaac.brigido@gmail.com>
2024-01-04 18:23:07 -0300

    incrementando e criando outros metodos para o processo seletivo

--- desafios/processo-seletivo/src/ProcessoSeletivo.java
+++ desafios/processo-seletivo/src/ProcessoSeletivo.java
@@ -1,9 +1,33 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
-        analisarCanditato(1999.0);
-        analisarCanditato(2000.0);
-        analisarCanditato(2500.0);
+        selecaoCandidatos();
 
+    }
+
+    static void selecaoCandidatos(){
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AGUSTO", "CHAVIER", "MONICA", "SCOTT", "LOGAN"};
+        int candidatosSelecionados = 0;
+        int contador = 0;
+        double salarioBase = 2000.0;
+
+        while (candidatosSelecionados < 3 && contador < candidatos.length ) {
+            String candidato = candidatos[contador];
+            double salario = salarioPretendido();
+
+            if(salarioBase >= salario){
+                candidatosSelecionados++;
+                System.out.println("O candidato " + candidato +" foi selecionado: ");
+            }
+            
+           contador++;
+        }
+
+    }
+
+    static double salarioPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800,2500);
     }
 
     static void analisarCanditato( double salarioPretendido){
